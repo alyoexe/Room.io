e9b0365c064dc1e8fe18e5b12b67c06677acfbfb
alex <alexanderalexeyo@gmail.com>
2025-06-12 23:38:33 +0530

    now deletes content when member leaves

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path("getToken/", views.getToken, name="getToken"),
     path('create_member/', views.createMember, name='create_member'),
     path('get_member/', views.getMember, name='get_member'),
-
+    path('delete_member/', views.deleteMember, name='delete_member'),
 
 
 ]
